2ac660461eb10c52c6aca4d8606ff74401999bd8
Victoria <sivosha2006@mail.ru>
2021-08-07 19:12:21 +0300

    added filter-method task

--- filter-find-map.js
+++ filter-find-map.js
@@ -17,3 +17,28 @@ function mergeArrays(arr1, arr2) {
 console.log(
   mergeArrays([1, 2, 3, 4, 8, 9, 10, 3, 7], [0, 3, 4, 5, 6, 7, 8, 9, 10, 11]) //the result will be [3,4,8,9,10,7]
 );
+
+
+//code-wars task:
+
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+//Some cases:
+
+//[22, -6, 32, 82, 9, 25] => [-6, 32, 25]
+
+//[68, -1, 1, -7, 10, 10] => [-1, 10]
+
+//[-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+//My solution:
+
+function multipleOfIndex(array) {
+  const multipleArray = array.filter((item, index, array) => {   //filter the array
+    if (item % index === 0) {                                    //if element of the array is multiple to its own index
+      return item;                                               //then return this element
+    }
+    return ""
+  });
+  return multipleArray;
+}
