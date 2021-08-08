00427a4d854c6695c805128ad302d7afff9df089
Victoria <sivosha2006@mail.ru>
2021-08-08 18:58:48 +0300

    added task for string methods

--- string-methods.js
+++ string-methods.js
@@ -9,3 +9,19 @@ function noSpace(x) {
     .join("");                          //make a string again
   return str;
 }
+
+
+//Given a string, return a copy of the string with the vowels' case swapped.
+
+//Example: Given a string "C is alive!", your function should return "C Is AlIvE!"
+
+//Please make sure you only swap cases for the vowels.
+
+
+//My solution:
+
+function swapVowelCase(str) {
+  return str.replace(/[aeiou]/gi, (x) =>                 //using method string replace with global flag and ignoring the case
+    /[AEIOU]/.test(x) ? x.toLowerCase() : x.toUpperCase() //second argument of this method can also be callback-function
+  );
+}
