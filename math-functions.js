c33fc0b29e05fa8e7f9e4eab434adb263fa9956e
Victoria <sivosha2006@mail.ru>
2021-08-12 13:32:07 +0300

    added math task

--- math-functions.js
+++ math-functions.js
@@ -7,3 +7,21 @@ function hexToDec(hexString) {
 }
 
 console.log(hexToDec("FF"));   //will retuen 255
+
+//Two red beads are placed between every two blue beads. There are N blue beads.
+//After looking at the arrangement below work out the number of red beads.
+
+//blue red red blue red red blue red red blue red red blue red red blue
+//If there are less than 2 blue beads return 0.
+
+//My solution:
+
+function countRedBeads(n) {
+  if (n < 2) {
+    return 0;
+  }
+  if (n > 2) {
+    return (n - 1) * 2;        //subtract 1 blue bead that comes at the very end, then the number of bead will be exactly two times higher
+  }
+}
+console.log(countRedBeads(5));     //will show 8
