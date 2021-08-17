205a71d22bb8015eb66ce9ae2e913e906d8e299a
Victoria <sivosha2006@mail.ru>
2021-08-17 20:35:33 +0300

    added task for array-mutating-methods

---
+++ array-mutating-methods.js
@@ -0,0 +1,17 @@
+//Given an array of integers your solution should find the smallest integer.
+
+//For example:
+
+    //Given [34, 15, 88, 2] your solution will return 2
+    //Given [34, -345, -1, 100] your solution will return -345
+
+//You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+//My solution:
+
+function findSmallestInt(args) {
+  const sortedArr = args.sort((a, b) => a - b);    //sort the array from smallest integers to bigger ones
+  const smallestInt = sortedArr.shift();           //delete first element (which is smallest integer now) of the array and save it into variable
+  return smallestInt;
+}
+console.log(findSmallestInt([78, 56, 232, 12, 8]));
