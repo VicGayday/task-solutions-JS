772f34d154d4944fe8209121bb272679f11e9825
Victoria <sivosha2006@mail.ru>
2021-10-05 22:11:15 +0300

    added loop task

--- loops.js
+++ loops.js
@@ -124,3 +124,16 @@ function containAllRots(str, arr) {
   }
   return true
 }
+
+//You are given a string of letters and an array of numbers.
+//The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+//After each removal the size of the string decreases (there is no empty space).
+//Return the only letter left.
+
+function lastSurvivor(letters, coords) {
+  letters = letters.split("");                  //transform string to array of letters
+  for (let i = 0; i < coords.length; i++) {
+    letters.splice(coords[i], 1);               //remove each letter according to coords array index, one by one
+  }
+  return letters.join("");                       //transform into string again
+}
